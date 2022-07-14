5a125f589b09ca894c19f7ddfbc42d9c98b7c1f0
Minsung Kim <epicblue@hanmail.net>
2022-07-15 01:53:52 +0900

    [test] 로컬에서 이미 PR된 브랜치로 PUSH 할 때

--- src/main/java/com/epicb/githubactionprac/infra/UserRepository.java
+++ src/main/java/com/epicb/githubactionprac/infra/UserRepository.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.epicb.githubactionprac.domain.model.User;
 
+// muyaho
 public interface UserRepository extends JpaRepository<User, Long> {
 }
